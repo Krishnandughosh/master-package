9417ed4025c7876a9e4a58a04a20366fbda39836
Broaj Ghosh <broja.ghosh@gmail.com>
2024-01-09 17:13:23 +0530

    initial code commit

---
+++ src/main/java/coreUtil/MasterUtil.java
@@ -0,0 +1,12 @@
+package coreUtil;
+
+public class MasterUtil {
+
+	public void baseMethod() {
+		System.out.println("Helow world");
+	}
+	
+	public void baseMethodAdvnce(String str) {
+		System.out.println("Helow world :: "+str);
+	}
+}
---
+++ src/main/java/coreUtil/mainclass.java
@@ -0,0 +1,13 @@
+package coreUtil;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MasterUtil mst=new MasterUtil();
+		mst.baseMethod();
+		mst.baseMethodAdvnce("Ram");
+
+	}
+
+}
